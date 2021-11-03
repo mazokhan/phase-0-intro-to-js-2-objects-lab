33d5dff141c7c1c5e533df89cf749ec8a5400412
mazen <mak9986@hotmail.com>
2021-11-03 21:08:24 +1100

    completed

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+// Write your solution in th Samis file!
+let employee ={ 
+    name:"test"
+    streetAddress : "test" 
+}
+
+function updatemployeewithkeyandvalue (employee, key, value){
+    let copy = {...employee};
+    copy[key] = value
+    return employee;
+
+    function destructivelyUpdateEmployeeWithKeyAndValue (employee,key,value){
+        employee[key] = value
+        return employee;
+    }
+
+    function deleteFromEmployeeByKey (employee, key){
+        let copy =object.assign({},employee)
+        delete copy [key]
+        return employee;
+    }
+
+    function destructivelyDeleteFromEmployeeByKey (employee,key){
+        delete employee[key]
+        return employee;
+    }
